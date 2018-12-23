9807d195dc28878b8ded9aeca3231606f099a93d
legkvla <vladimir.legkunets@gmail.com>
2018-12-23 21:30:05 +0300

    Copying client code from another project

---
+++ src/helda_cli/client/entities.clj
@@ -0,0 +1,23 @@
+(ns helda-cli.client.entities
+  (:require
+    [clj-http.client :as client]
+    )
+  )
+
+(defn prepare-entity [world model entity]
+  (cond-> entity
+    true (assoc :world (name world))
+    true (assoc :model (name model))
+    (-> model :tags nil?) (assoc :tags [])
+    )
+  )
+
+(defn save-entity [world model entity]
+  (client/post
+    "http://localhost:3000/entities/save-entity"
+    {
+      :form-params (prepare-entity world model entity)
+      :content-type :json
+      }
+    )
+  )
---
+++ src/helda_cli/client/models.clj
@@ -0,0 +1,25 @@
+(ns helda-cli.client.models
+  (:require
+    [clj-http.client :as client]
+    )
+  )
+
+(defn prepare-model [package model-name model]
+  (cond-> model
+    true (assoc :full-name (str (name package) "." (name model-name)))
+    true (assoc :package (name package))
+    true (assoc :name (name model-name))
+    (-> model :extends nil?) (assoc :extends [])
+    (-> model :actions nil?) (assoc :actions {})
+    )
+  )
+
+(defn save-model [package name model]
+  (client/post
+    "http://localhost:3000/models/save-model"
+    {
+      :form-params (prepare-model package name model)
+      :content-type :json
+      }
+    )
+  )
